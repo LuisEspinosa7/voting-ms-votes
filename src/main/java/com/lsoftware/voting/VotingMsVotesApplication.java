6b9b68872dbd68a12a5bbc7da1db873cd3c6dc88
Luis Espinosa <lluisllanoss@gmail.com>
2022-05-08 21:07:21 -0500

    Consult service discovery and api gateway

--- src/main/java/com/lsoftware/voting/VotingMsVotesApplication.java
+++ src/main/java/com/lsoftware/voting/VotingMsVotesApplication.java
@@ -3,8 +3,10 @@ package com.lsoftware.voting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableAutoConfiguration
 public class VotingMsVotesApplication {
 
